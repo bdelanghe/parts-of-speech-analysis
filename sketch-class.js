0b968e04add041076572c1e335a664e888be8092
Robert DeLanghe <bdelanghe@gmail.com>
2017-06-10 19:40:30 -0400

    Created a word object.

---
+++ sketch-class.js
@@ -0,0 +1,91 @@
+// Job Listing POS
+// Robert DeLanghe
+// http://robertdelanghe.com
+// version: '0.0.2'
+
+function setup(){
+  noCanvas();
+}
+
+var i = (''), rs = [], ra = [], words = [], counts = [];
+
+var word = class word {
+  constructor(string, pos, freq, count) {
+    this.string = string,
+    this.pos = pos,
+    this.freq = freq,
+    this.count = count;
+  }
+}
+
+function input(){
+  i = document.getElementById("TextBox").value;
+  // console.log(i);
+
+  document.getElementById("button1").firstChild.data = 'processing...';
+  document.getElementById("TextBox").value = '';
+
+  a = tokens(i);
+  c = count(a);
+  // console.log(c);
+  p = POS(c[0]);
+  console.log(p);
+  // console.log(a);
+  // console.log(a.sort());
+
+  document.getElementById("button1").firstChild.data = 'complete!';
+
+  return i;
+}
+
+function tokens(s) {
+  rs = new RiString(s);
+  ra = rs.words();
+  return ra;
+}
+
+function POS(words) {
+  pos = [];
+  for (var w = 0; w < words.length; w++) {
+    var rita = new RiString(str(w));
+    var p = rita.pos();
+    pos.push(p);
+  }
+  return [words, pos];
+}
+
+
+function freq(){
+
+}
+
+function count(s){
+  words = s.sort();
+  arr = [];
+  cnt = [];
+  for (var w = 0; w < words.length; w++) {
+    var lc = words[w].toLowerCase();
+    if (!arr.includes(lc)) {
+      arr.push(lc);
+      cnt.push(1);
+    } else {
+      c = cnt[arr.length - 1];
+      c += 1;
+      cnt[arr.length - 1] = c;
+    }
+  }
+  counts = cnt;
+  return [arr, cnt];
+}
+
+// function duplicates(){
+//
+// }
+
+function print(){
+
+}
+
+// i = input();
+// wordarray = tokens(i);
+// console.log(wordarray);
