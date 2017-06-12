ee9b39ce617fc83aae1c74af8465729c7d727364
Stan Rozenraukh <stan@stanistan.com>
2017-06-12 19:09:36 -0400

    cleanup or w/e

--- sketch-class.js
+++ sketch-class.js
@@ -3,179 +3,142 @@
 // http://robertdelanghe.com
 // version: '0.0.2'
 
-function setup(){
-  noCanvas();
-}
-
-var i = (''), rs = [], ra = [], words = [], counts = [];
-
-var pt = ['cc', 'cd', 'dt', 'ex', 'fw', 'in', 'jj', 'jjr', 'jjs', 'ls', 'md', 'nn', 'nns', 'nnp', 'nnps', 'pdt', 'pos', 'prp', 'prp$', 'rb', 'rbr', 'rbs', 'rp', 'sym', 'to', 'uh', 'vb', 'vbd', 'vbg', 'vbn', 'vbp', 'vbz', 'wdt', 'wp', 'wp$', 'wrb'];
-var posdes = ['Coordinating conjunction','Cardinal number','Determiner','Existential there','Foreign word','Preposition or subordinating conjunction','Adjective','Adjective, comparative','Adjective, superlative','List item marker','Modal','Noun, singular or mass','Noun, plural','Proper noun, singular','Proper noun, plural','Predeterminer','Possessive ending','Personal pronoun','Possessive pronoun','Adverb','Adverb, comparative','Adverb, superlative','Particle','Symbol','to','Interjection','Verb, base form','Verb, past tense','Verb, gerund or present participle','Verb, past participle','Verb, non-3rd person singular present','Verb, 3rd person singular present','Wh-determiner','Wh-pronoun','Possessive wh-pronoun','Wh-adverb'];
-var show = [false,false,false,false,false,false,true,false,false,false,false,true,true,true,true,false,false,true,true,false,false,false,false,false,false,false,true,true,true,true,true,true,false,false,false,false];
-
-var word = class word {
-  constructor(string, pos, freq, count) {
-    this.string = string,
-    this.pos = pos,
-    this.freq = freq,
-    this.count = count;
-  }
-}
-
-function createwords(s, pos, freq, count) {
-  arr = [];
-  for (var w = 0; w < s.length; w++) {
-    var word = {
-      string:s[w],
-      pos:pos[w],
-      freq:freq[w],
-      count:count[w]
-    }
-    arr.push(word);
-  }
-  return arr;
-}
-
-function wordarray(input){
-  a = tokens(i);
-
-  cs = count(a);
-  c = cs[1];
-  s = cs[0];
-  f = freq(s);
-  p = POS(s);
-
-  arr = createwords(s,p,f,c);
-  return arr;
-}
-
-
-function input(){
-  i = document.getElementById("TextBox").value;
-
-  while (div1.hasChildNodes()) {
-  div1.removeChild(div1.lastChild);
-  }
-
-  document.getElementById("button1").firstChild.data = 'processing...';
-  document.getElementById("TextBox").value = '';
-
-  var array = wordarray(i);
-  var groups = group_pos(array);
-  var head = drawbody(groups);
-
-  document.getElementById("button1").firstChild.data = 'complete!';
-
-
-  return i;
-}
-
-function tokens(s) {
-  rs = new RiString(s);
-  ra = rs.words();
-  return ra;
-}
-
-function POS(s) {
-  pos = [];
-  for (var w = 0; w < s.length; w++) {
-    rs = new RiString(str(s[w]));
-    var p = rs.pos();
-    pos.push(p[0]);
-  }
-  return pos;
-}
-
-
-function freq(s){
-  arr = [];
-  for (var w = 0; w < s.length; w++) {
-    var ind = wfreq.indexOf(s[w]);
-    if (ind == -1) {
-      arr.push(null);
-      } else {
-      rank = rfreq[ind];
-      arr.push(rank);
-    }
-  }
-  return arr;
-}
-
-function count(s){
-  words = s.sort();
-  arr = [];
-  cnt = [];
-  for (var w = 0; w < words.length; w++) {
-    var lc = words[w].toLowerCase();
-    if (!arr.includes(lc)) {
-      arr.push(lc);
-      cnt.push(1);
-    } else {
-      c = cnt[arr.length - 1];
-      c += 1;
-      cnt[arr.length - 1] = c;
-    }
-  }
-  return [arr, cnt];
-}
-
-// function duplicates(){
 //
+// You can store all of the parts of speech
+// as an array of dictionaries, so that it is
+// ordered in consistently _and_ declarative.
+//
+// I took the previous code and in the js console, ran:
+//
+// ```js
+// var o = [];
+// for (var i = 0; i < pt.length; i++) {
+//   o.push({ pos: pt[i], name: posdes[i] });
 // }
+// JSON.stringify(o);
+// ```
+//
+var POS_CONFIG = [
+  {"pos":"cc","name":"Coordinating conjunction"},
+  {"pos":"cd","name":"Cardinal number"},
+  {"pos":"dt","name":"Determiner"},
+  {"pos":"ex","name":"Existential there"},
+  {"pos":"fw","name":"Foreign word"},
+  {"pos":"in","name":"Preposition or subordinating conjunction"},
+  {"pos":"jj","name":"Adjective"},
+  {"pos":"jjr","name":"Adjective, comparative"},
+  {"pos":"jjs","name":"Adjective, superlative"},
+  {"pos":"ls","name":"List item marker"},
+  {"pos":"md","name":"Modal"},
+  {"pos":"nn","name":"Noun, singular or mass"},
+  {"pos":"nns","name":"Noun, plural"},
+  {"pos":"nnp","name":"Proper noun, singular"},
+  {"pos":"nnps","name":"Proper noun, plural"},
+  {"pos":"pdt","name":"Predeterminer"},
+  {"pos":"pos","name":"Possessive ending"},
+  {"pos":"prp","name":"Personal pronoun"},
+  {"pos":"prp$","name":"Possessive pronoun"},
+  {"pos":"rb","name":"Adverb"},
+  {"pos":"rbr","name":"Adverb, comparative"},
+  {"pos":"rbs","name":"Adverb, superlative"},
+  {"pos":"rp","name":"Particle"},
+  {"pos":"sym","name":"Symbol"},
+  {"pos":"to","name":"to"},
+  {"pos":"uh","name":"Interjection"},
+  {"pos":"vb","name":"Verb, base form"},
+  {"pos":"vbd","name":"Verb, past tense"},
+  {"pos":"vbg","name":"Verb, gerund or present participle"},
+  {"pos":"vbn","name":"Verb, past participle"},
+  {"pos":"vbp","name":"Verb, non-3rd person singular present"},
+  {"pos":"vbz","name":"Verb, 3rd person singular present"},
+  {"pos":"wdt","name":"Wh-determiner"},
+  {"pos":"wp","name":"Wh-pronoun"},
+  {"pos":"wp$","name":"Possessive wh-pronoun"},
+  {"pos":"wrb","name":"Wh-adverb"}
+];
 
-function group_pos(arr) {
-  var groups = [];
-  for (var p = 0; p < pt.length; p++) {
-    // console.log('looping: ', pt[p]);
-    var group = [];
-    for (var s = arr.length - 1; s >= 0; s--) {
-      // console.log(arr[s].string, ' : ', arr[s].pos, ' : ', pt[p]);
-      var x = arr[s];
-      if(x.pos === pt[p]) {
-        // console.log('true');
-        group.push(x.string);
-        arr.splice(s, 1);
+function analyzeText() {
+
+  //
+  // Declaring all of the variables I'm going to be using
+  // in the scope of this execution...
+  var textInput = document.getElementById("TextBox").value,
+      output = document.getElementById("div1"),
+      rawWords = (new RiString(textInput)).words(),
+      wordMap = {},
+      posMap = {};
+
+  //
+  // Here, I'm going through every word, this `forEach` is
+  // pretty similar to doing a `for loop`, but takes care of that
+  // for you, you can write a function as an argument to `forEach`
+  // (called a callback).
+  //
+  // This person may have explained this better than me:
+  // http://eloquentjavascript.net/05_higher_order.html
+  //
+  // This goes through each of the words and stores them
+  // by their normalized (lowercase) keys in a dictionary
+  // so that we only have one `Word` object for each of them,
+  // we can update the count when we see the word again.
+  //
+  rawWords.forEach(function(word) {
+    var key = word.toLowerCase();
+    if (key in wordMap) {
+      wordMap[key].count++;
+    } else {
+      // Similarly to how this is storing the value in
+      // `wordMap`, we can create a `posMap` and check if
+      // this is a part of speech we care about _right here_
+      // and then store that word in it, instead of doing the
+      // many walkthroughs this wordMap object later when we're
+      // looking through the POS_CONFIG
+      //
+      // I leave that to you...
+      //
+      // ¯\_(ツ)_/¯
+      var partsOfSpeech = (new RiString(key)).pos();
+      wordMap[key] = {
+        word: key,
+        count: 1,
+        pos: partsOfSpeech
+      };
+    }
+  });
+
+  // instead of walking each of the children of this DOM node,
+  // it's pretty easy to clear this out with the `innerText` property
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/innerText
+  output.innerHTML = ''; // clear it out
+
+  // Given that we've defined this Parts of Speech configuration above,
+  // we can walk it similarly to the words, and check if each
+  // of the words belongs to a POS that we want to display
+  POS_CONFIG.forEach(function(POS) {
+    var words = [];
+    for (var key in wordMap) {
+      var word = wordMap[key];
+      if (word.pos.includes(POS.pos)) {
+        words.push(word.word);
       }
     }
-    groups.push(group);
-    // console.log(group);
-  }
-  return groups;
-}
 
-function drawbody(a) {
-  for (var i = 0; i < a.length; i++) {
-    if (a[i].length != 0 && show[i]){
-      var head = document.createElement('h2');
-      var headnode = document.createTextNode(posdes[i]);
-      head.appendChild(headnode);
-      var element = document.getElementById("div1");
-      element.appendChild(head);
+    // If there are any words for this POS,
+    // we sort them by their count and then do the
+    // DOM node creation.
+    if (words.length) {
+      words.sort(function(a, b) {
+        return wordMap[a].count >= wordMap[b].count ? -1 : 1;
+      });
 
-      t = ('');
-      for (var j = 0; j < a[i].length; j++) {
-        g = a[i];
-        if (j == 0) {
-          t += g[j];
-        } else {
-          t += ', ';
-          t += g[j];
-        }
-      }
-      var para = document.createElement("p");
-      var node = document.createTextNode(t);
-      para.appendChild(node);
-      element.appendChild(para);
-      t = ('');
+      // This can be prettier, the DOM api isn't the most succinct.
+      var content = document.createElement("div");
+      var heading = document.createElement("h2");
+      heading.appendChild(document.createTextNode(POS.name));
+      content.appendChild(heading);
+      content.appendChild(document.createElement("p").appendChild(document.createTextNode(words.join(", "))));
+      div1.appendChild(content);
     }
-  }
-  //   var head = document.createElement('h2');
-  //   var headnode = document.createTextNode(des);
-  //   var para = document.createElement("p");
-  //   var node = document.createTextNode(lst);
-  //   head.appendChild(headnode);
-  //   para.appendChild(node);
-  //   var element = document.getElementById("div1");
-  //   element.appendChild(head);
-  //   element.appendChild(para);
+  });
+
 }
